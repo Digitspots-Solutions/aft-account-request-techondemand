25fe1ca820abf1e506f039a2157b6601d71f6c3f
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-12 22:05:03 -0400

    Add account request for oludolabank-admin (admin@oludolabank.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -175,3 +175,33 @@ module "preciouscosmeticshub-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "oludolabank-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@oludolabank.com"
+    AccountName               = "oludolabank-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-jpw6-2gskotwy)"
+    SSOUserEmail              = "admin@oludolabank.com"
+    SSOUserFirstName          = "oludolabank"
+    SSOUserLastName           = "oludolabank"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-13T02:05:03.270595"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
