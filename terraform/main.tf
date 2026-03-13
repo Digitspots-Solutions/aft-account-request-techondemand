170b9e149ba80861ccd007dfdbe9473c397c4931
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-12 22:06:58 -0400

    Add account request for markethives-admin (admin@markethives.org)

--- terraform/main.tf
+++ terraform/main.tf
@@ -265,3 +265,33 @@ module "fablommechandise-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "markethives-org-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@markethives.org"
+    AccountName               = "markethives-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-jpw6-2gskotwy)"
+    SSOUserEmail              = "admin@markethives.org"
+    SSOUserFirstName          = "markethives"
+    SSOUserLastName           = "markethives"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-13T02:06:58.087052"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
