98cc7d69e033fe8c5320be4d31d40485da34a243
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-12 22:08:14 -0400

    Add account request for sylvykrisssuperstore-admin (admin@sylvykrisssuperstore.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -325,3 +325,33 @@ module "queenstarcomputerserviceandenterprises-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sylvykrisssuperstore-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@sylvykrisssuperstore.com"
+    AccountName               = "sylvykrisssuperstore-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-jpw6-2gskotwy)"
+    SSOUserEmail              = "admin@sylvykrisssuperstore.com"
+    SSOUserFirstName          = "sylvykrisssuperstore"
+    SSOUserLastName           = "sylvykrisssuperstore"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-13T02:08:14.327805"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
