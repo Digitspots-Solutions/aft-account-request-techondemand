59eb544fff61acef3095dd5512d588bf5351b977
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-12 22:11:56 -0400

    Add account request for dewiseautomechanic-admin (admin@dewiseautomechanic.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -415,3 +415,33 @@ module "vikiniaenterprise-com-admin" {
 
   account_customizations_name = "sandbox"
 }
+
+module "dewiseautomechanic-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@dewiseautomechanic.com"
+    AccountName               = "dewiseautomechanic-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-jpw6-2gskotwy)"
+    SSOUserEmail              = "admin@dewiseautomechanic.com"
+    SSOUserFirstName          = "dewiseautomechanic"
+    SSOUserLastName           = "dewiseautomechanic"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-13T02:11:56.221574"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
