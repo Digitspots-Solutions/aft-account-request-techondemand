410e6033b20827ff4824f15be5375556b84e4713
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2026-03-12 22:00:18 -0400

    Add account request for berrycbakeryandsalad-admin (admin@berrycbakeryandsalad.com)

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,32 @@ module "sandbox" {
   account_customizations_name = "sandbox"
 }
 
+module "berrycbakeryandsalad-com-admin" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "admin@berrycbakeryandsalad.com"
+    AccountName               = "berrycbakeryandsalad-admin"
+    ManagedOrganizationalUnit = "Websites Workload (ou-jpw6-2gskotwy)"
+    SSOUserEmail              = "admin@berrycbakeryandsalad.com"
+    SSOUserFirstName          = "berrycbakeryandsalad"
+    SSOUserLastName           = "berrycbakeryandsalad"
+  }
+
+  account_tags = {
+    "Opportunity" = "true"
+    "ManagedBy" = "OpportunityPortal"
+    "ProvisioningRetry" = "2026-03-13T02:00:18.377428"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Opportunity Portal"
+    change_reason       = "Self-service account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
